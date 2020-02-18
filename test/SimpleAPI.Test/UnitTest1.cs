d4e613dbe646348b94432178dd90e347d82d90ba
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2020-02-18 14:46:16 +0600

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsCorrectNumber()
+        {
+        var returnValue = controller.Get(1);
+        Assert.Equal("Sanjoy Kumar Das", returnValue.Value);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
